3cb051b64ecfc5d23620aeb63e50201fe23b3c75
jun6292 <c68254@gmail.com>
2024-05-23 18:03:21 +0900

    메멘토 패턴 적용

---
+++ MementoPattern/Memento.java
@@ -0,0 +1,19 @@
+package MementoPattern;
+
+public class Memento {
+    private double x;
+    private double y;
+
+    public Memento(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
